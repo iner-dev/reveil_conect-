6c404659e5ee58bce2eb27153bfab3f88cbe1abe
Iner <inerdenormandie@gmail.com>
2026-03-02 17:22:57 +0100

    Remove hardcoded WiFi credentials for the 3.2V

--- code/snapshot_V3.2/reveil/local_parameters.h
+++ code/snapshot_V3.2/reveil/local_parameters.h
@@ -5,8 +5,6 @@
 #define Ical_URL "your Ical URL"
 inline void define_wifi(WIFI_manager* WIFI_ls){ // from 1 to 4 wifi lines 
     WIFI_ls->add_wifi("your WIFI SSID 1","your WIFI PSW 1");
-    WIFI_ls->add_wifi("your WIFI SSID 2","your WIFI PSW 2");
-    WIFI_ls->add_wifi("your WIFI SSID 3","your WIFI PSW 3");
-}
+    WIFI_ls->add_wifi("your WIFI SSID 2","your WIFI PSW 2");}
 
 #endif
